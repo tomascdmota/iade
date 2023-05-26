179613445da93ebdf1b18ff223449e446d334dcd
tomascdmota <tomasmota@outlook.com>
2023-05-26 22:43:56 +0100

    final

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-import React from 'react'
-import ReactDOM from "react-dom"
-import './index.css'
-import App from './App'
-import { hydrate, render } from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
 
-const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-  hydrate(<App />, rootElement);
-} else {
-  render(<App />, rootElement);
-}
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
